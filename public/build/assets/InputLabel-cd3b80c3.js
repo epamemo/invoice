e4969d56e72e01d9782870896a93b30f3678b896
epamemo <epamemo@gmail.com>
2023-08-07 14:15:13 +0700

    new build

---
+++ public/build/assets/InputLabel-cd3b80c3.js
@@ -0,0 +1,1 @@
+import{a as n}from"./app-434eec2a.js";function r({value:t,className:e="",children:a,...m}){return n("label",{...m,className:"block font-medium text-sm "+e,children:t||a})}export{r as I};
